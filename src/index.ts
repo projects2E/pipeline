1fda47c5ecb601a40371ae9634af941ee56509ae
Jorge Arango <projects@eande.com.co>
2024-02-04 11:54:13 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// index.ts
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  const customText = process.env.API_APP;
+  res.json({env: customText});
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
