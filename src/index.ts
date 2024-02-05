ceb7860a005ccc5b2dd6b6c5b7f8ac37622760b2
Jorge Arango <projects@eande.com.co>
2024-02-05 13:29:53 -0500

    novedad

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   const customText = process.env.API_APP;
-  res.json({env: customText, data: "tODAy"});
+  res.json({env: customText, data: "tODAy", nasd: "asdasd"});
 });
 
 app.listen(port, () => {
