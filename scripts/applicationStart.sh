4aea10efd78047fc4cc3cbbd7b6292e3615b369c
Jorge Arango <projects@eande.com.co>
2024-02-05 13:18:07 -0500

    final project

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Iniciar PM2 con la aplicación
 cd projects/pipeline
-pm2 start ecosystem.config.js 
+~/.nvm/versions/node/v21.6.1/bin/pm2 start ecosystem.config.js
--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Iniciar PM2 con la aplicación
-cd projects/pipeline
-~/.nvm/versions/node/v21.6.1/bin/pm2 start ecosystem.config.js
+~/.nvm/versions/node/v21.6.1/bin/pm2 stop all
