686ec2716ea6d5d318325c45a67eeb8df9e7e45f
Jorge Arango <projects@eande.com.co>
2024-02-05 13:10:22 -0500

    update script

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-# Soporte para instalar nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-# Configura nvm en el script
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Esto carga nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Esto carga la finalización de bash para nvm
-
-# Instala Node.js 20 con nvm
-nvm install 20
-
-# Instala pm2 globalmente
-npm install pm2 -g
-
-# Detiene todos los procesos pm2
-pm2 stop all
+# Iniciar PM2 con la aplicación
+cd projects/pipeline
+~/.nvm/versions/node/v21.6.1/bin/pm2 start ecosystem.config.js
